e2499ca64599c4c0a762fb94a75a9f5cab9fd20b
Kalasour <nuntawat_wisedsup@cmu.ac.th>
2020-01-19 00:05:16 +0700

    fix grabpush bug and create sword with speed damage

---
+++ CodingVR/Assets/AttachPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachPlatform : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player) {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
---
+++ CodingVR/Assets/Sword.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Sword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float damage = 10;
+    public TextMeshPro Label;
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         damage = rigidbody.velocity.magnitude*10;
+        Label.text = "Damage : " + ((int)damage).ToString();
+    }
+}
