d0e04efe1c437281c84f583e9d30864408802305
Kalasour <nuntawat_wisedsup@cmu.ac.th>
2020-01-27 14:20:01 +0700

    add hp bar

--- CodingVR/Assets/playerControlCustom.cs
+++ CodingVR/Assets/playerControlCustom.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class playerControlCustom : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public SimpleHealthBar healthBar;
     void Start()
     {
 
     }
 
     // Update is called once per frame
-    void Update()
+    public bool isLife() {
+        return healthBar.GetCurrentFraction > 0;
+    }
+    void FixedUpdate()
     {
-
-            
     }
 }
