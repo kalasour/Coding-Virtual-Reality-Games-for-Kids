63837be07353b6b4bb4d8f2ff1d3956f51e5e8ee
Kalasour <nuntawat_wisedsup@cmu.ac.th>
2020-02-04 22:46:10 +0700

    create boolean

---
+++ CodingVR/Assets/Scripts/Programming/Block Subclasses/Boolean.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+public class Boolean : Block
+{
+    public string var;
+    public Text childText;
+    public bool value = false;
+    override public bool CheckCond()
+    {
+        // Debug.Log(get.getDistance());
+        // Debug.Log(get.getDistance());
+
+        return value;
+    }
+    // private void Start() {
+    // 	childText.text=name;
+    // }
+    private void FixedUpdate()
+    {
+        if(value)
+            gameObject.GetComponent<Image>().color = new Color32(255, 0, 0, 255);
+        else
+            gameObject.GetComponent<Image>().color = new Color32(0, 0, 225, 255);
+
+    }
+
+    override protected void CreateConnections()
+    {
+        // var = childText.text;
+        this.blockType = BlockType.BlockTypeLogic;
+        Connection previousConnection = new Connection(this, new Vector2(50, 50), Connection.ConnectionType.Previous);
+
+        previousConnection.SetAcceptableBlockType(BlockType.BlockTypeInscrution | BlockType.BlockTypeConditionJoint);
+
+        this.connections.Add(previousConnection);
+
+    }
+}
