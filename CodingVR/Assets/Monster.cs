898cf24518176a320ba8c9cc5783c0f59827f204
Kalasour <nuntawat_wisedsup@cmu.ac.th>
2020-02-07 04:59:48 +0700

    done for present nsc1

---
+++ CodingVR/Assets/Monster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animation_Test Anim;
+    public TextMeshPro HP;
+    private float hp = 100;
+    void Start()
+    {
+        Anim = gameObject.GetComponent<Animation_Test>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (hp <= 0 && !Anim.isDeath)
+        {
+            HP.SetText("");
+            Anim.DeathAni();
+        }
+        else {
+            HP.SetText((int)hp + "%");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "sword" && Anim.isCanAttack()) {
+            float damage = collision.gameObject.GetComponent<Sword>().damage;
+            hp -= damage;
+            Anim.DamageAni();
+        }
+    }
+}
---
+++ CodingVR/Assets/toggleArea.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleArea : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject targetTrigger;
+    public string booString;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == targetTrigger) {
+            GameObject[] found = GameObject.FindGameObjectsWithTag("Block");
+            foreach (GameObject i in found) {
+                if (i.GetComponent<Boolean>() != null)
+                {
+                    Boolean boo = i.GetComponent<Boolean>();
+                    if (boo.ID == booString) {
+                        boo.value = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == targetTrigger)
+        {
+            GameObject[] found = GameObject.FindGameObjectsWithTag("Block");
+            foreach (GameObject i in found)
+            {
+                if (i.GetComponent<Boolean>() != null)
+                {
+                    Boolean boo = i.GetComponent<Boolean>();
+                    if (boo.ID == booString)
+                    {
+                        boo.value = false;
+                    }
+                }
+            }
+        }
+    }
+}
