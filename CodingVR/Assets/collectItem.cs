4bf96a760ca791f19e00df645c0af7beb13a6869
Kalasour <nuntawat_wisedsup@cmu.ac.th>
2020-02-10 21:36:45 +0700

    can grab item when mosnter died

---
+++ CodingVR/Assets/collectItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            if (target != null) {
+                target.SetActive(true);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
