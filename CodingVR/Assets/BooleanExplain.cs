aa18134de3814d52e63c81068f099e865d6e90ff
Kalasour <nuntawat_wisedsup@cmu.ac.th>
2020-02-10 20:24:35 +0700

    created BooleanExplain on area

---
+++ CodingVR/Assets/BooleanExplain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class BooleanExplain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isOn;
+    TextMeshPro text;
+    void Start()
+    {
+        text = gameObject.GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOn) {
+            text.color = new Color32(255, 255, 255, 255);
+        }
+        else {
+            text.color = new Color32(255, 255, 255, 0);
+        }
+    }
+}
