b3eeb4a0e9997e906ba227d3a2a80010a3fb1716
Kalasour <nuntawat_wisedsup@cmu.ac.th>
2020-04-18 19:52:54 +0700

    add instruction to many object in scene_2

---
+++ CodingVR/Assets/AreaCon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaCon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject last;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject Go = transform.GetChild(i).gameObject;
+            if (last == null) last = Go;
+            if (Go.GetComponent<toggleArea>().state && Go != last)
+            {
+                last.SetActive(true);
+                last = Go;
+                Go.SetActive(false);
+            }
+        }
+    }
+}
---
+++ CodingVR/Assets/attakcer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attakcer : MonoBehaviour
+{
+    public Transform target;
+    public GameObject projectile;
+
+    public float maximumLookDistance = 30;
+    public float maximumAttackDistance = 10;
+    public float minimumDistanceFromPlayer = 2;
+
+    public float rotationDamping = 2;
+
+    public float shotInterval = 0.5f;
+
+    private float shotTime = 0;
+
+    void Update()
+    {
+        var distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= maximumLookDistance)
+        {
+            LookAtTarget();
+
+            //Check distance and time
+            //if (distance <= maximumAttackDistance && (Time.time - shotTime) > shotInterval)
+           // {
+           //     Shoot();
+           // }
+        }
+    }
+
+    public bool canShoot() {
+        var distance = Vector3.Distance(target.position, transform.position);
+        return (distance <= maximumAttackDistance && (Time.time - shotTime) > shotInterval);
+    }
+    void LookAtTarget()
+    {
+        var dir = target.position - transform.position;
+        dir.y = 0;
+        var rotation = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
+    }
+
+
+    public void Shoot()
+    {
+        //Reset the time when we shoot
+        shotTime = Time.time;
+        Instantiate(projectile, transform.position + (target.position - transform.position).normalized, Quaternion.LookRotation(target.position - transform.position));
+    }
+}
