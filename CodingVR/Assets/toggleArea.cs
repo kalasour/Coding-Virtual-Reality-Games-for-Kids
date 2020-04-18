77a613ce14ca1d60a4d90064b5ac0568eb7fc520
kalasour <nuntawat_wisedsup@cmu.ac.th>
2020-04-19 04:24:01 +0700

    done env scene_3

--- CodingVR/Assets/toggleArea.cs
+++ CodingVR/Assets/toggleArea.cs
@@ -2,89 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class toggleArea : MonoBehaviour
-{
+public class toggleArea : MonoBehaviour {
     // Start is called before the first frame update
     public GameObject targetTrigger;
     public BooleanExplain targetText;
     public string booString;
     public bool state;
-    void Start()
-    {
+    void Start () {
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update () {
 
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject == targetTrigger)
-        {
-            GameObject[] found = GameObject.FindGameObjectsWithTag("Block");
-            foreach (GameObject i in found)
-            {
-                if (i.GetComponent<Boolean>() != null)
-                {
-                    Boolean boo = i.GetComponent<Boolean>();
-                    if (boo.ID == booString)
-                    {
+    private void OnTriggerEnter (Collider other) {
+        // if (other.gameObject == targetTrigger)
+        if (true) {
+            GameObject[] found = GameObject.FindGameObjectsWithTag ("Block");
+            foreach (GameObject i in found) {
+                if (i.GetComponent<Boolean> () != null) {
+                    Boolean boo = i.GetComponent<Boolean> ();
+                    if (boo.ID == booString) {
                         state = true;
                         boo.value = true;
                     }
                 }
             }
         }
-        if (other.gameObject.CompareTag("Player") && targetText != null)
-        {
+        if (other.gameObject.CompareTag ("Player") && targetText != null) {
             targetText.isOn = true;
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == targetTrigger)
-        {
-            GameObject[] found = GameObject.FindGameObjectsWithTag("Block");
-            foreach (GameObject i in found)
-            {
-                if (i.GetComponent<Boolean>() != null)
-                {
-                    Boolean boo = i.GetComponent<Boolean>();
-                    if (boo.ID == booString)
-                    {
+    private void OnTriggerExit (Collider other) {
+        // if (other.gameObject == targetTrigger)
+        if (true) {
+            GameObject[] found = GameObject.FindGameObjectsWithTag ("Block");
+            foreach (GameObject i in found) {
+                if (i.GetComponent<Boolean> () != null) {
+                    Boolean boo = i.GetComponent<Boolean> ();
+                    if (boo.ID == booString) {
                         state = false;
                         boo.value = false;
                     }
                 }
             }
         }
-        if (other.gameObject.CompareTag("Player") && targetText != null)
-        {
+        if (other.gameObject.CompareTag ("Player") && targetText != null) {
             targetText.isOn = false;
         }
     }
 
-    private void OnDisable()
-    {
-        GameObject[] found = GameObject.FindGameObjectsWithTag("Block");
-        foreach (GameObject i in found)
-        {
-            if (i.GetComponent<Boolean>() != null)
-            {
-                Boolean boo = i.GetComponent<Boolean>();
-                if (boo.ID == booString)
-                {
+    private void OnDisable () {
+        GameObject[] found = GameObject.FindGameObjectsWithTag ("Block");
+        foreach (GameObject i in found) {
+            if (i.GetComponent<Boolean> () != null) {
+                Boolean boo = i.GetComponent<Boolean> ();
+                if (boo.ID == booString) {
                     state = false;
                     boo.value = false;
                 }
             }
         }
-        if (targetText != null)
-        {
+        if (targetText != null) {
             targetText.isOn = false;
         }
     }
--- CodingVR/Assets/walkToPlayer.cs
+++ CodingVR/Assets/walkToPlayer.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class attakcer : MonoBehaviour {
+public class walkToPlayer : MonoBehaviour {
     private Transform target;
-    public GameObject projectile;
 
     public float maximumLookDistance = 30;
     public float maximumAttackDistance = 10;
@@ -15,7 +14,8 @@ public class attakcer : MonoBehaviour {
     public float shotInterval = 0.5f;
 
     private float shotTime = 0;
-    void Start () {
+    public float speed = 3;
+    private void Start () {
         target = GameObject.FindGameObjectWithTag ("Player").transform;
     }
     void Update () {
@@ -23,32 +23,17 @@ public class attakcer : MonoBehaviour {
 
         if (distance <= maximumLookDistance) {
             LookAtTarget ();
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards (transform.position, target.position, step);
 
-            //Check distance and time
-            //if (distance <= maximumAttackDistance && (Time.time - shotTime) > shotInterval)
-            // {
-            //     Shoot();
-            // }
         }
+
     }
 
-    public bool canShoot () {
-        var distance = Vector3.Distance (target.position, transform.position);
-        return (distance <= maximumAttackDistance && (Time.time - shotTime) > shotInterval);
-    }
     void LookAtTarget () {
         var dir = target.position - transform.position;
         dir.y = 0;
         var rotation = Quaternion.LookRotation (dir);
         transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationDamping);
-    }
-
-    public void Shoot () {
-        //Reset the time when we shoot
-        shotTime = Time.time;
-        GameObject clone = Instantiate (projectile);
-        clone.GetComponent<Bullet> ().speed = 100;
-        clone.transform.localScale *= 3;
-        Instantiate (clone, transform.position + (target.position - transform.position).normalized, Quaternion.LookRotation (target.position - transform.position));
     }
 }
