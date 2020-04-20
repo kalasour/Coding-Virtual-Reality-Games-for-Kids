4e9ce78794bde0df7b8507cb98d4874a7d13a884
kalasour <nuntawat_wisedsup@cmu.ac.th>
2020-04-20 07:00:14 +0700

    beatiful scene_4

--- CodingVR/Assets/Bullet.cs
+++ CodingVR/Assets/Bullet.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private Transform target;
+    public Transform target;
     public float speed = 3;
     private void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        if (target == null) target = GameObject.FindGameObjectWithTag("Player").transform;
         Destroy(gameObject, 5f);
     }
     void Update()
     {
         float step = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
     }
 }
---
+++ CodingVR/Assets/onDestroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onDestroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject explored;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// This function is called when the MonoBehaviour will be destroyed.
+    /// </summary>
+    void OnDestroy()
+    {
+        if (explored != null)
+        {
+            Instantiate(explored, transform.position, explored.transform.rotation).SetActive(true);
+        }
+    }
+}
